fd19786697600180ff7634ce19010c008f1d8b9f
francoaccifonte <franco.accifonte@gmail.com>
2017-06-07 13:39:15 -0300

    Update touch_read.asm

--- codigo/touch_read.asm
+++ codigo/touch_read.asm
@@ -1,9 +1,5 @@
 	;.include "m328def.inc"
 
-;Constantes de calibración:
-	.equ CAL_X	=	0x00378F66
-	.equ CAL_Y	=	0x03C34155
-	.equ CAL_S	=	0x000EF13F
 ;-----------------------------------------------
 ; Recibo en r16 la orden que se le envia al ADC y devuelvo en r17(high) y r18(low) lo que leyo el ADC
 ; las ordenes que se le envia son:
@@ -42,16 +38,6 @@
 	;.equ T_BUSY=	32		;no mapeado
   ;Pin del DOUT
 	.equ PIN_DOUT =	PINB
-
-;Constantes magicas que hacen a la calibracion
-
-	.equ orient			=	1 ; portrait o landscape
-	.equ touch_x_left	=	(CAL_X>>14) & 0x3FF;
-	.equ touch_x_right	=	CAL_X & 0x3FFF;
-	.equ touch_y_top	=	(CAL_Y>>14) & 0x3FFF;
-	.equ touch_y_bottom	=	CAL_Y & 0x3FFF;
-	.equ disp_x_size	=	(CAL_S>>12) & 0x0FFF;
-	.equ disp_y_size	=	CAL_S & 0x0FFF;
 
 ;Seteo puertos de salida y entrada, e inicializo el CS en high, y el resto en low
 
